b39382f500ea45581aacbda1b1960f785a64618d
umrahzadi <umrahzadi.sw18@muetkhp.edu.pk>
2022-09-09 13:36:22 +0100

    final

---
+++ app/src/main/java/com/example/socialmediaintegration/FacebookActivity.java
@@ -0,0 +1,53 @@
+package com.example.socialmediaintegration;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FacebookActivity extends AppCompatActivity {
+    EditText username;
+    EditText password,email;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fb);
+
+        username= (EditText) findViewById(R.id.username);
+        password= (EditText) findViewById(R.id.password);
+        email= (EditText) findViewById(R.id.email);
+        btn= (Button) findViewById(R.id.loginbtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = username.getText().toString().trim();
+                String text2 = password.getText().toString().trim();
+                String text3 = email.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("key1", text);
+                bundle.putString("key2", text2);
+                bundle.putString("key3", text3);
+                Intent intent = new Intent(FacebookActivity.this, SecondActivity.class);
+                intent.putExtras(bundle);
+                if (text.trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "plz Enter username", Toast.LENGTH_SHORT).show();
+                }else if (text2.trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "plz Enter password", Toast.LENGTH_SHORT).show();
+                } else if (text3.trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "plz Enter email", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "login successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                }
+            }
+
+        });
+    }
+
+}
--- app/src/main/java/com/example/socialmediaintegration/MainActivity.java
+++ app/src/main/java/com/example/socialmediaintegration/MainActivity.java
@@ -2,53 +2,44 @@ package com.example.socialmediaintegration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
-         EditText username;
-         EditText password,email;
-         Button btn;
-
+    ImageView imggoogle,imgfb,imginsta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imggoogle=findViewById(R.id.imggoogle);
+        imgfb=findViewById(R.id.imgfb);
+        imginsta=findViewById(R.id.imginsta);
 
-        username= (EditText) findViewById(R.id.username);
-        password= (EditText) findViewById(R.id.password);
-        email= (EditText) findViewById(R.id.email);
-        btn= (Button) findViewById(R.id.loginbtn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+       imggoogle.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent= new Intent(getApplicationContext(), GoogleActivity.class);
+               startActivity(intent);
+           }
+       });
+        imgfb.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String text = username.getText().toString().trim();
-                String text2 = password.getText().toString().trim();
-                String text3 = email.getText().toString().trim();
-                Bundle bundle = new Bundle();
-                bundle.putString("key1", text);
-                bundle.putString("key2", text2);
-                bundle.putString("key3", text3);
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtras(bundle);
-                if (text.trim().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "plz Enter username", Toast.LENGTH_SHORT).show();
-                }else if (text2.trim().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "plz Enter password", Toast.LENGTH_SHORT).show();
-                } else if (text3.trim().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "plz Enter email", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "login successfully", Toast.LENGTH_SHORT).show();
-                    startActivity(intent);
-                }
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), FacebookActivity.class);
+                startActivity(intent);
             }
-
         });
-    }
+        imginsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), InstagramActivity.class);
+                startActivity(intent);
+            }
+        });
 
+    }
 }
